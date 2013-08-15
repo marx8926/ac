d975f5184ff8bfbf3c0734ee8c12f10bd344e052
Marks <artmar89@gmail.com>
2013-08-15 09:30:11 -0500

    database

--- config/routes.rb
+++ config/routes.rb
@@ -4,7 +4,7 @@ Ac::Application.routes.draw do
 
   # You can have the root of your site routed with "root"
  
- root 'welcome#index'
+  # root 'welcome#index'
 
   # Example of regular route:
   #   get 'products/:id' => 'catalog#view'
